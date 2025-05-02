906c392248fe43a7fbaee06e0d9d6b8fe95fd8c0
p2r3 <41925384+p2r3@users.noreply.github.com>
2025-05-03 01:09:08 +0300

    fix: left-pad computed millisecond value

--- main.js
+++ main.js
@@ -48,7 +48,9 @@ function updateTimeClock () {
   elemTime.hours.value = Math.floor(seconds / 3600) || "";
   elemTime.minutes.value = Math.floor(seconds % 3600 / 60) || "";
   elemTime.seconds.value = Math.floor(seconds % 3600 % 60) || "";
-  elemTime.milliseconds.value = Math.round(seconds % 3600 % 60 * 1000 % 1000) || "";
+  let milliseconds = Math.round(seconds % 3600 % 60 * 1000 % 1000) || "";
+  while (milliseconds.length < 3) milliseconds = "0" + milliseconds;
+  elemTime.milliseconds.value = milliseconds;
 }
 
 function sanitizeInput (event) {
